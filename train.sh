85dfff131b3d3789555a489cd019a45f82e36b13
Pratyush Maini <pratyush.maini@gmail.com>
2018-11-10 00:33:05 +0530

    Kill working FINALLYYYY

--- train.sh
+++ train.sh
@@ -14,14 +14,13 @@ do
     python client.py 0.0.0.0 $var ./run.sh &
     sleep 0.1s
     python client.py 0.0.0.0 $var $i -mode GUI 
-    
-    pkill -9 python
-    # source kill_python.sh
     sleep 1s
+    pkill -9 python
+    
     let "var++"
     echo $var
     echo Awake
-    # pkill -9 python
+    
     tail -1 Learn/log11.txt | head -1 >> Learn/log55.txt
 
     echo Next
@@ -29,13 +28,13 @@ do
     echo 5,5 Player 2, Opp = $i >> Learn/log55.txt
     python server.py $var -n 5 -s 5 -NC 2 -TL 120 -LOG server.log > Learn/log12.txt&
     sleep 0.1s
-    python client.py 0.0.0.0 $var $i -mode GUI 
+    python client.py 0.0.0.0 $var $i -mode GUI &
     sleep 0.1s
-    python client.py 0.0.0.0 $var ./run.sh &
-    
+    python client.py 0.0.0.0 $var ./run.sh 
+    sleep 1s
     pkill -9 python
     # source kill_python.sh
-    sleep 1s
+    
     let "var++"
     echo $var
     echo Awake
@@ -50,10 +49,10 @@ do
     python client.py 0.0.0.0 $var ./run.sh &
     sleep 0.1s
     python client.py 0.0.0.0 $var $i -mode GUI 
-    
+    sleep 1s
     pkill -9 python
     # source kill_python.sh
-    sleep 1s
+    
     let "var++"
     echo $var
     echo Awake
@@ -65,13 +64,13 @@ do
     echo 6,5 Player 2, Opp = $i >> Learn/log65.txt
     python server.py $var -n 6 -s 5 -NC 2 -TL 150 -LOG server.log > Learn/log22.txt&
     sleep 0.1s
-    python client.py 0.0.0.0 $var $i -mode GUI
+    python client.py 0.0.0.0 $var $i -mode GUI &
     sleep 0.1s
-    python client.py 0.0.0.0 $var ./run.sh &
-    
+    python client.py 0.0.0.0 $var ./run.sh 
+    sleep 1s
     pkill -9 python
     # source kill_python.sh
-    sleep 1s
+    
     let "var++"
     echo $var
     echo Awake
@@ -86,10 +85,10 @@ do
     python client.py 0.0.0.0 $var ./run.sh &
     sleep 0.1s
     python client.py 0.0.0.0 $var $i -mode GUI 
-    
+    sleep 1s
     pkill -9 python
     # source kill_python.sh
-    sleep 1s
+    
     let "var++"
     echo $var
     echo Awake
@@ -101,13 +100,13 @@ do
     echo 6,6 Player 2, Opp = $i >> Learn/log66.txt
     python server.py $var -n 6 -s 6 -NC 2 -TL 180 -LOG server.log > Learn/log32.txt&
     sleep 0.1s
-    python client.py 0.0.0.0 $var $i -mode GUI 
+    python client.py 0.0.0.0 $var $i -mode GUI &
     sleep 0.1s
-    python client.py 0.0.0.0 $var ./run.sh &
-    
+    python client.py 0.0.0.0 $var ./run.sh 
+    sleep 1s
     pkill -9 python
     # source kill_python.sh
-    sleep 1s
+    
     let "var++"
     echo $var
     echo Awake
